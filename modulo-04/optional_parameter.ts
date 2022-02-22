d1e2828396827a0a56ce5f552d92f5ae89b81a2d
Luis Filipe Guedes Mota <filipemot@gmail.com>
2022-02-22 15:33:49 -0300

    Aula 46 - TypeScript - Zero to Hero | Demo - Optional Parameters #45

--- modulo-04/optional_parameter.ts
+++ modulo-04/optional_parameter.ts
@@ -9,6 +9,38 @@ function informacaoDadosPessoas(
     console.log('Email ... : ', email);
   }
 }
+
 informacaoDadosPessoas(1, 'João');
 informacaoDadosPessoas(2, 'João', 'teste@teste.com.br');
 
+function mensagemLog(mensagem: string, usuarioId?: number){
+  const horaLog = new Date().toLocaleTimeString();
+  console.log(horaLog, mensagem, usuarioId || ' Usuário não conectado!');
+}
+
+mensagemLog('Atualizar Página!');
+mensagemLog('Usuário Conectado com Sucesso!', 1);
+
+type Pessoa = {
+  idFuncionario: number;
+  nome: string;
+  idade?: number;
+  email?: string;
+};
+
+let pessoa: Pessoa;
+pessoa = {
+  idFuncionario: 1,
+  nome: 'João',
+};
+
+console.log(pessoa);
+
+pessoa = {
+  idFuncionario: 1,
+  nome: 'João',
+  idade: 25,
+  email: 'teste@teste.com'
+};
+
+console.log(pessoa);
