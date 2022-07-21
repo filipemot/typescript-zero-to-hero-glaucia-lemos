d194830fef0363208e1db52ddc777645d3be967b
Luis Filipe Guedes Mota <filipemot@gmail.com>
2022-07-21 18:45:19 -0300

    Aula 56 - TypeScript - Zero to Hero | Demo - Modificadores de Acesso: readonly #55

--- modulo-05/readonly.ts
+++ modulo-05/readonly.ts
@@ -1,3 +1,5 @@
+export {};
+//Emeplo 01
 class Funcionario {
   readonly dataNascimento: Date;
 
@@ -9,4 +11,48 @@ class Funcionario {
 const funcionario = new Funcionario(new Date(1988, 5, 15));
 //funcionario.dataNascimento = new Date(1988, 5, 15); //Erro de compilação
 
+// Exemplo 02
 console.log(funcionario.dataNascimento);
+
+class Funcionario_01 {
+  constructor(readonly dataNascimento: Date) {
+    this.dataNascimento = dataNascimento;
+  }
+}
+
+//Exemplo 03
+class Funcionario_02 {
+
+  nome: string;
+  readonly codigoFuncionario: number;
+
+  constructor(nome: string, codigoFuncionario: number) {
+    this.nome = nome;
+    this.codigoFuncionario = codigoFuncionario;
+  }
+}
+
+const funcionario02 = new Funcionario_02('Glaucia Lemos', 8967);
+funcionario02.nome = 'Glaucia Lemos';
+//funcionario02.codigoFuncionario = 8967;//Erro de compilação
+
+//Emeplo 04
+interface IFuncionario {
+  codigoFuncionario: number;
+  nomeEmpregado: string;
+}
+
+const funcionario03: Readonly<IFuncionario> = {
+  codigoFuncionario: 8967,
+  nomeEmpregado: 'Glaucia Lemos',
+};
+
+//funcionario03.nomeEmpregado = 'Glaucia Lemos';//Erro de compilação
+
+const funcionario04: IFuncionario = {
+  codigoFuncionario: 8967,
+  nomeEmpregado: 'Glaucia Lemos',
+};
+
+funcionario04.nomeEmpregado = 'Glaucia Lemos';
+
